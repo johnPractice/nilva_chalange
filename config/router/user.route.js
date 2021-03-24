08016f126d19b143dfa3af7eeac0e7a95758739b
Reza Mousavian <rezamosavian1010@gmail.com>
2021-03-24 09:25:04 +0430

    feat : create server

--- config/router/user.route.js
+++
@@ -1,8 +0,0 @@
-const Router = require("express").Router;
-const route = Router();
-const UserController = require("../../src/controllers/UserController");
-
-route.post("/signup", UserController.signUp);
-route.post("/login", UserController.login);
-
-module.exports = route;
