d4ed59231270820e9cb3748b6271a4a8a2e7ef7f
Reza Mousavian <rezamosavian1010@gmail.com>
2021-03-24 09:58:32 +0430

    fea(Model) : add question model

---
+++ src/models/LongAnswerQuestion.js
@@ -0,0 +1,35 @@
+const Mongoose = require("mongoose");
+const Schema = Mongoose.Schema;
+
+const schema = new Schema(
+  {
+    question: {
+      type: String,
+      required: [true, "must enter requires thing"],
+    },
+    answerUser: {
+      type: String,
+    },
+    answer: {
+      type: String,
+      required: [true, "must enter requires thing"],
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+schema.methods.toJSON = function () {
+  var obj = this.toObject();
+  delete obj.createdAt;
+  delete obj.updatedAt;
+  delete obj.__v;
+  delete obj._id;
+  return obj;
+};
+
+const model = Mongoose.model("longAnswerQuestion", schema);
+module.exports = model;
