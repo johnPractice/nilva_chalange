9815028ab459855ea7bb18d5c84e9ec98f154b40
Reza Mousavian <rezamosavian1010@gmail.com>
2021-03-25 23:50:30 +0430

    feat : add basic config for socket

--- config/app.js
+++ config/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const setRoutes = require("./routes");
 const databaseConnect = require("./database");
 const { handleError } = require("../src/helpers/AppError");
+const cors = require("cors");
 const app = express();
 databaseConnect();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 setRoutes(app);
 app.use((err, req, res, next) => {
   handleError(err, res);
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const http = require("http");
 const app = require("./config/app");
 const server = http.createServer(app);
 const { port } = require("./config");
-
+const io = require("socket.io")(server);
+const WebSockets = require("./src/utils/socket/webSocket");
+global.io = io.listen(server);
+global.io.on("connection", WebSockets.connection);
 server.listen(port, () => {
   console.clear();
   console.log(`server runnig on ${port} 😇 `);
